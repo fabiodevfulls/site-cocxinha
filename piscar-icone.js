74620050c71981cddbf6aa1571661bcb8d934b61
fabiodevfulls <122120480+fabiodevfulls@users.noreply.github.com>
2023-01-12 08:58:58 -0300

    Create piscar-icone.js

---
+++ piscar-icone.js
@@ -0,0 +1,9 @@
+setInterval(function() {
+    var icone = document.getElementById('icone-whatsapp');
+    if (icone.style.display === 'none') {
+      icone.style.display = 'block';
+    } else {
+      icone.style.display = 'none';
+    }
+  }, 2000);
+  
